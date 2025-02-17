599d8a141fb9682b4679e206b17060882efb08fa
kuber-2003 <kuberbansal26@gmail.com>
2025-02-17 12:18:19 +0530

    Lecture 6

---
+++ lecture6/script.js
@@ -0,0 +1,64 @@
+// console.log(1);
+// console.log(sumOfTwo(10,5));
+// function sumOfTwo(a,b){
+//     return a+b;
+// }
+// setTimeout(console.log(4),2000);
+// var start = 1;
+// var end = 100000000;
+// while(start<end){
+//     start++;
+// }
+// console.log("End");
+// setInterval(console.log(9),4000);
+
+
+// console.log(1);
+// console.log(sumOfTwo(10,5));
+
+// function sumOfTwo(a, b) {
+//     return a + b;
+// }
+
+// // Corrected setTimeout
+// setTimeout(() => console.log(4), 2000);
+
+// var start = 1;
+// var end = 1000000000;
+// while (start < end) {
+//     start++;
+// }
+
+// console.log("End");
+
+// // Corrected setInterval
+// setInterval(() => console.log(9), 4000);
+
+
+//Functions
+
+// 1. Named function
+// xyz(10,5);
+// function xyz(a,b){
+//     console.log(a+b);
+// }
+
+// // 2. anonymous function
+// function anonymous(){
+//     console.log("hello");
+// }
+
+// // 3. arrow function
+
+// const add = (a, b) => a + b;
+// console.log(add(5, 3)); // Output: 8
+
+
+xyz();
+abc();
+function xyz(){
+    console.log(1);
+}
+var abc = function(){
+    console.log(2);
+}
